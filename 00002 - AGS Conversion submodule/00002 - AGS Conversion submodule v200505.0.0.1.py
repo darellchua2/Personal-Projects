70167eefb69d303fb822a5b70754730b778793ae
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-05-06 19:13:02 +0800

    Added New Function FindIndexInCSVToSplit(file)
    added new function to Find the index of splitting 1 csv file to multiple files

--- 00002 - AGS Conversion submodule/00002 - AGS Conversion submodule v200505.0.0.1.py
+++ 00002 - AGS Conversion submodule/00002 - AGS Conversion submodule v200505.0.0.1.py
@@ -7,7 +7,28 @@ import shutil
 import pandas as pd
 from csv import DictReader
 
-# 
+def FindIndexInCSVToSplit(file):
+	index = 0
+	start_index = 0
+	end_index = 0
+	new_dict = {}
+	substring = "**"
+	with open(file) as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			try:
+				if substring in row[0]:
+					store = row[0]
+					start_index = index
+					print("this is start index " + str(start_index))
+			except IndexError as error:
+				print("Empty row is at " + str(index))
+				end_index = index
+				new_dict[store] = [start_index,end_index]
+			finally: 
+				index +=1
+	return new_dict
+
 def CreateFileWithNewExtension(file,destination_folder):
 	try: 
 		currDir = os.getcwd()
@@ -18,8 +39,8 @@ def CreateFileWithNewExtension(file,destination_folder):
 		print(file + " has been copied over to " + destination_folder + " with a change in extension")
 		shutil.copyfile(file,output_file)
 	except FileExistsError as error:
-                    print(file + " File is not copy")
-                    pass
+					print(file + " File is not copy")
+					pass
 
 def CreateFileWithNewExtension(file,destination_folder,source_extension,destination_extension):
 	try: 
@@ -33,63 +54,52 @@ def CreateFileWithNewExtension(file,destination_folder,source_extension,destinat
 		print(file + " has been copied over to " + destination_folder + " with a change in extension")
 		shutil.copyfile(file,output_file)
 	except FileExistsError as error:
-                        print(file + " File is not copy")
-                        pass
+						print(file + " File is not copy")
+						pass
 
 def createFolder(folder_name):
 	try:
-	    os.mkdir(folder_name)
+		os.mkdir(folder_name)
 	except OSError:
-	    print ("Creation of the directory %s failed, the file already exist" % folder_name)    
+		print ("Creation of the directory %s failed, the file already exist" % folder_name)    
 
 def PrintCSVRowList(row):
 	try:
-	    print(row[0])
+		print(row[0])
 	except IndexError as error:
-	    print("There is no index here")
+		print("There is no index here")
 
 createFolder("AGS TO csv - Compilation")
 # CreateFileWithNewExtension(file,"AGS TO csv - Compilation",".ags",".csv")	
 currDir = os.getcwd()
 
 for subdir, dirs, files in os.walk(currDir):
-    for file in files:
-        base_file, ext = os.path.splitext(file)
-        filepath = subdir + os.sep + file
-        print(filepath)
-        if filepath.endswith(".ags"):
-        	CreateFileWithNewExtension(file,"AGS TO csv - Compilation",".ags",".csv")
+	for file in files:
+		base_file, ext = os.path.splitext(file)
+		filepath = subdir + os.sep + file
+		print(filepath)
+		if filepath.endswith(".ags"):
+			CreateFileWithNewExtension(file,"AGS TO csv - Compilation",".ags",".csv")
 
 
-file = "1-SGO_SI_ROM.csv"
-file2 = open(file)
-reader = csv.reader(file2)
-lines = len(list(reader))
-# print(lines)
 
 
-# open file in read mode
+print(FindIndexInCSVToSplit("5-SGO SI LIM CHU KANG SINGAPORE.csv"))
 
-csvfile = open("5-SGO SI LIM CHU KANG SINGAPORE.csv", 'r')
-csvreader = csv.reader(csvfile)
-index = 0
-start_index = 0
-end_index = 0
-new_dict = dict()
-
-for row in csvreader:
-	if(start_index == 0):
-		print(row)
-	if(len(row)==0):
-		print(start_index, end_index)
-		print("Empty row is at " + str(index))
-		end_index = index
-
-		if(start_index == 0):
-			new_dict = {"hi2":[start_index,end_index]}
-		start_index = index + 1
-
-
-	index += 1
-
-print(new_dict)
+# index = 0
+# with open("5-SGO SI LIM CHU KANG SINGAPORE.csv") as csvfile:
+# 		reader = csv.reader(csvfile)
+# 		for row in reader:
+# 			try:
+# 				if substring in row[0]:
+# 					store = row[0]
+# 					start_index = index
+# 					print("this is start index " + str(start_index))
+# 			except IndexError as error:
+# 				print("Empty row is at " + str(index))
+# 				end_index = index
+# 				new_dict[store] = [start_index,end_index]
+# 			finally: 
+# 				index +=1
+# 	return new_dict
+		
