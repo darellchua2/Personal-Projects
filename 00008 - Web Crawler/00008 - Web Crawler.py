f4e12c310fd4601f2263ca203b3537dc1d47ec84
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-05-17 14:38:22 +0800

    Resolved certain issue

--- 00008 - Web Crawler/00008 - Web Crawler.py
+++ 00008 - Web Crawler/00008 - Web Crawler.py
@@ -1,35 +1,88 @@
 import bs4 as bs
 import urllib.request
-import urllib
-import csv
-
-source = urllib.request.urlopen('https://yugioh.fandom.com/wiki/Set_Card_Lists:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)').read()
-
-soup = bs.BeautifulSoup(source,'lxml')
-print(soup.encode("utf-8"))
-
-f = csv.writer(open("OUTPUT.csv", "w", encoding="utf-8"))
-
-links = soup.find_all('a')
-
-for link in links:
-    print(link)
-    f.writerow([str(link)])
-
-import urllib
-from urllib.request import Request, urlopen
 import csv
 import requests
 from PIL import Image
 from io import BytesIO
+import os
+import pandas as pd
 
-url = 'https://vignette.wikia.nocookie.net/yugioh/images/a/a6/TenThousandDragon-IGAS-JP-10000ScR.png/revision/latest/scale-to-width-down/118?cb=20191025013811'
 
-urllib.request.urlretrieve(url, "pic1.png")
+def CreateFolder(folder_name):
+    try:
+        os.mkdir(folder_name)
+    except OSError:
+        print ("Creation of the directory '%s' failed, the file already exist" % folder_name)
 
-response = requests.get(url)
-name = f"test.png"
-filename = f"./downloads/{name}"
-img = Image.open(BytesIO(response.content))
-img.save(filename)
-img = ""
+
+def DownloadImage(url,filename,ext = "png"):
+    createFolder(("downloads"))
+    output_filename = filename + "." + ext
+    urllib.request.urlretrieve(url, output_filename)
+    response = requests.get(url)
+    name = f"test.png"
+    filename = f"./downloads/{name}"
+    img = Image.open(BytesIO(response.content))
+    img.save(filename)
+    img = ""
+
+
+def OutputHTMLFileSummary(url,html_tag,output_file):
+    a = []
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+    # print(soup.encode("utf-8"))
+    f = csv.writer(open(output_file, "w", encoding="utf-8"))
+    links = soup.find_all(html_tag)
+    for link in links:
+        print(link)
+        if link == a:
+            pass
+        else:
+            try:
+                f.writerow([link.name,link.a,link.a.next_sibling,str(link)])
+            except:
+                f.writerow([link.name,link.a,str(link)])
+
+def OutputHTMLFileSummaryIMG(url,html_tag,output_file):
+    a = []
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+    # print(soup.encode("utf-8"))
+    f = csv.writer(open(output_file, "w", encoding="utf-8"))
+    links = soup.find_all(html_tag)
+    for link in links:
+        print(link)
+        if link == a:
+            pass
+        else:
+            f.writerow([link.name, link.title, link["src"], link.text, str(link)])
+
+url1 = 'https://yugioh.fandom.com/wiki/Set_Card_Lists:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
+url2 = 'https://yugioh.fandom.com/wiki/Set_Card_Galleries:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
+
+html_tag = "tr"
+output_file1 = "DBMF - CardList - tr.csv" #change this to your own file output
+output_file2 = "DBMF - CardGallery - img.csv" #change this to your own file output
+output_file3 = "DBMF - CardGallery - img2.csv" #change this to your own file output
+output_file4 = "DBMF - CardList - a.csv" #change this to your own file output
+
+
+# source = urllib.request.urlopen(url).read()
+# soup = bs.BeautifulSoup(source,'lxml')
+# print(soup.encode("utf-8"))
+# # links = soup.find_all(html_tag, id=False)
+# links = soup.find_all(html_tag)
+#
+# for link in links:
+#     print(link.name,link.text)
+
+# OutputHTMLFileSummary(url1,"tr",output_file1)
+# OutputHTMLFileSummary(url2,"img",output_file2)
+# OutputHTMLFileSummaryIMG(url2,"img",output_file3)
+OutputHTMLFileSummary(url2,"a",output_file4)
+
+
+
+
+
