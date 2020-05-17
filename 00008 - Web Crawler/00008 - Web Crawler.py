164e2703a756deed18888d9cb99eefb50ab622ac
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-05-17 21:41:21 +0800

    test1

--- 00008 - Web Crawler/00008 - Web Crawler.py
+++ 00008 - Web Crawler/00008 - Web Crawler.py
@@ -30,24 +30,20 @@ def DownloadImage(url,filename,ext = "png"):
 def OutputHTMLFileSummary(url,html_tag,output_file):
     a = []
     source = urllib.request.urlopen(url).read()
-    soup = bs.BeautifulSoup(source, 'lxml')
+    soup = bs.BeautifulSoup(source, 'html.parser')
     # print(soup.encode("utf-8"))
     f = csv.writer(open(output_file, "w", encoding="utf-8"))
     links = soup.find_all(html_tag)
+    counter = 0.0
     for link in links:
+        counter +=1
         print(link)
-        if link == a:
-            pass
-        else:
-            try:
-                f.writerow([link.name,link.a,link.a.next_sibling,str(link)])
-            except:
-                f.writerow([link.name,link.a,str(link)])
+        f.writerow([str(link)])
 
 def OutputHTMLFileSummaryIMG(url,html_tag,output_file):
     a = []
     source = urllib.request.urlopen(url).read()
-    soup = bs.BeautifulSoup(source, 'lxml')
+    soup = bs.BeautifulSoup(source, 'html.parser')
     # print(soup.encode("utf-8"))
     f = csv.writer(open(output_file, "w", encoding="utf-8"))
     links = soup.find_all(html_tag)
@@ -57,32 +53,46 @@ def OutputHTMLFileSummaryIMG(url,html_tag,output_file):
             pass
         else:
             f.writerow([link.name, link.title, link["src"], link.text, str(link)])
-
+    f.close()
 url1 = 'https://yugioh.fandom.com/wiki/Set_Card_Lists:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
-url2 = 'https://yugioh.fandom.com/wiki/Set_Card_Galleries:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
-
-html_tag = "tr"
-output_file1 = "DBMF - CardList - tr.csv" #change this to your own file output
-output_file2 = "DBMF - CardGallery - img.csv" #change this to your own file output
-output_file3 = "DBMF - CardGallery - img2.csv" #change this to your own file output
-output_file4 = "DBMF - CardList - a.csv" #change this to your own file output
-
-
-# source = urllib.request.urlopen(url).read()
-# soup = bs.BeautifulSoup(source,'lxml')
-# print(soup.encode("utf-8"))
-# # links = soup.find_all(html_tag, id=False)
-# links = soup.find_all(html_tag)
+# url2 = 'https://yugioh.fandom.com/wiki/Set_Card_Galleries:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
 #
-# for link in links:
-#     print(link.name,link.text)
+# html_tag = "tr"
+output_file1 = "DBMF - CardList - tr.csv" #change this to your own file output
+# output_file2 = "DBMF - CardGallery - img.csv" #change this to your own file output
+# output_file3 = "DBMF - CardGallery - img2.csv" #change this to your own file output
+# output_file4 = "DBMF - CardList - a.csv" #change this to your own file output
+# output_file5 = "DBMF - CardList - td.csv" #change this to your own file output
+#
+#
+#
+# # source = urllib.request.urlopen(url).read()
+# # soup = bs.BeautifulSoup(source,'lxml')
+# # print(soup.encode("utf-8"))
+# # # links = soup.find_all(html_tag, id=False)
+# # links = soup.find_all(html_tag)
+# #
+# # for link in links:
+# #     print(link.name,link.text)
+#
+OutputHTMLFileSummary(url1,"tr",output_file1)
+# # OutputHTMLFileSummary(url2,"img",output_file2)
+# # OutputHTMLFileSummaryIMG(url2,"img",output_file3)
+# OutputHTMLFileSummary(url2,"a",output_file4)
+# OutputHTMLFileSummary(url2,"td",output_file5)
 
-# OutputHTMLFileSummary(url1,"tr",output_file1)
-# OutputHTMLFileSummary(url2,"img",output_file2)
-# OutputHTMLFileSummaryIMG(url2,"img",output_file3)
-OutputHTMLFileSummary(url2,"a",output_file4)
+output_file1_2 = "DBMF - CardList - tr2.csv" #change this to your own file output
 
+# with open(output_file1,"r",encoding="utf-8") as in_file:
+#     with open(output_file1_2, 'w',encoding="utf-8") as out_file:
+#         writer = csv.writer(out_file)
+#         for row in csv.reader(in_file):
+#             if row:
+#                 print(type(row),len(row),row[0])
+#                 new_row = row[0].replace("<a>",",")
+#                 print(new_row)
 
-
-
+array = []
+for item in soup.findAll('tr', {'valign': 'top'}):
+     array.append([f.text.strip().replace("\xa0\n\t", "") for f in item.findAll("td")])
 
