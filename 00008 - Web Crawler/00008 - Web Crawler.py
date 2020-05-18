a66eccf08219575bdb5ce67a115fc0e48af184c8
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-05-18 19:02:24 +0800

    Scrapping Success IMG

--- 00008 - Web Crawler/00008 - Web Crawler.py
+++ 00008 - Web Crawler/00008 - Web Crawler.py
@@ -6,6 +6,8 @@ from PIL import Image
 from io import BytesIO
 import os
 import pandas as pd
+import re
+
 
 
 def CreateFolder(folder_name):
@@ -53,36 +55,15 @@ def OutputHTMLFileSummaryIMG(url,html_tag,output_file):
             pass
         else:
             f.writerow([link.name, link.title, link["src"], link.text, str(link)])
-    f.close()
+
+
 url1 = 'https://yugipedia.com/wiki/Set_Card_Lists:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
 url2 = 'https://yugipedia.com/wiki/Set_Card_Galleries:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
 #
 # html_tag = "tr"
 output_file1 = "DBMF - CardList - tr1.csv" #change this to your own file output
-# output_file2 = "DBMF - CardGallery - img.csv" #change this to your own file output
-# output_file3 = "DBMF - CardGallery - img2.csv" #change this to your own file output
-# output_file4 = "DBMF - CardList - a.csv" #change this to your own file output
-# output_file5 = "DBMF - CardList - td.csv" #change this to your own file output
 output_file6 = "DBMF - CardList - tr1.csv" #change this to your own file output
-
-#
-#
-#
-# # source = urllib.request.urlopen(url).read()
-# # soup = bs.BeautifulSoup(source,'lxml')
-# # print(soup.encode("utf-8"))
-# # # links = soup.find_all(html_tag, id=False)
-# # links = soup.find_all(html_tag)
-# #
-# # for link in links:
-# #     print(link.name,link.text)
-#
-# OutputHTMLFileSummary(url1,"tr",output_file1)
-# # OutputHTMLFileSummary(url2,"img",output_file2)
-# # OutputHTMLFileSummaryIMG(url2,"img",output_file3)
-# OutputHTMLFileSummary(url2,"a",output_file4)
-# OutputHTMLFileSummary(url2,"td",output_file5)
-
+output_file7 = "DBMF - CardGallery - img.csv" #change this to your own file output
 output_file1_2 = "DBMF - CardList - tr2.csv" #change this to your own file output
 
 
@@ -98,11 +79,36 @@ def OutputCardList(url,html_tag,output_file,sub_tag):
         counter += 1
         lists.append([f.text.strip().replace("\xa0\n\t", "") for f in link.find_all(sub_tag)])
     for i in range(len(lists)):
-        print(i)
         if i/2 != 0 and i>1:
             lists[i][1] = lists[i][1].strip('"')
             df.loc[i] = lists[i]
-            df.to_csv(output_file6)
+            df.to_csv(output_file)
     print(df)
 
-OutputCardList(url1,"tr",output_file6,"td")
+# OutputCardList(url1,"tr",output_file6,"td")
+
+def OutputHTMLFileSummaryIMG2(url,html_tag,output_file,check_string):
+    df = pd.DataFrame(columns = ("Card Name","Card URL"))
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'html.parser')
+    f = csv.writer(open(output_file, "w", encoding="utf-8"))
+    links = soup.find_all(html_tag)
+    counter = 0
+    for i in range(len(links)):
+        # print(type(tag["src"]))
+        img_src = links[i]["src"]
+        if check_string in img_src:
+            img_src = img_src.replace('/thumb','')
+            img_src_splitlist = img_src.rsplit('/',2)
+            card_name = img_src_splitlist[1].split('-',1)
+            card_name[0] = re.sub(r"(\w)([A-Z])", r"\1 \2", card_name[0])
+            new_list = [card_name[0],img_src_splitlist[0] + "/" + img_src_splitlist[1]]
+            print(new_list)
+            df.loc[counter] = new_list
+            counter += 1
+    df.to_csv(output_file)
+    print(df)
+    return df
+
+OutputHTMLFileSummaryIMG2(url2,"img",output_file7,"DBMF")
+
