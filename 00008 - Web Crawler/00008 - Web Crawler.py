36ad5f81ecfb72d6ad6a20f50a1334c3b3b87cb3
Khiong Kiat Chua <e0021787@u.nus.edu>
2020-05-18 16:21:59 +0800

    Added Method - OutputCardList():
    Finalized Method for Output CardList as proper CSV

--- 00008 - Web Crawler/00008 - Web Crawler.py
+++ 00008 - Web Crawler/00008 - Web Crawler.py
@@ -54,8 +54,8 @@ def OutputHTMLFileSummaryIMG(url,html_tag,output_file):
         else:
             f.writerow([link.name, link.title, link["src"], link.text, str(link)])
     f.close()
-url1 = 'https://yugioh.fandom.com/wiki/Set_Card_Lists:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
-# url2 = 'https://yugioh.fandom.com/wiki/Set_Card_Galleries:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
+url1 = 'https://yugipedia.com/wiki/Set_Card_Lists:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
+url2 = 'https://yugipedia.com/wiki/Set_Card_Galleries:Deck_Build_Pack:_Mystic_Fighters_(OCG-JP)'
 #
 # html_tag = "tr"
 output_file1 = "DBMF - CardList - tr1.csv" #change this to your own file output
@@ -85,73 +85,25 @@ output_file6 = "DBMF - CardList - tr1.csv" #change this to your own file output
 
 output_file1_2 = "DBMF - CardList - tr2.csv" #change this to your own file output
 
-# with open(output_file1,"r",encoding="utf-8") as in_file:
-#     with open(output_file1_2, 'w',encoding="utf-8") as out_file:
-#         writer = csv.writer(out_file)
-#         for row in csv.reader(in_file):
-#             if row:
-#                 print(type(row),len(row),row[0])
-#                 new_row = row[0].replace("<a>",",")
-#                 print(new_row)
 
-
-
-def OutputHTMLFileSummary2(url,html_tag,output_file):
-    array = []
+def OutputCardList(url,html_tag,output_file):
+    lists = []
+    counter = 0
+    df = pd.DataFrame(columns = ("CODE","Card Name","Card Name(Japanese)", "Rarity","Card Type","Status"))
     source = urllib.request.urlopen(url).read()
     soup = bs.BeautifulSoup(source, 'html.parser')
     f = csv.writer(open(output_file, "w", encoding="utf-8"))
     links = soup.find_all(html_tag)
-
-    counter = 0.0
     for link in links:
         counter += 1
-        if (counter/2) != 0.0:
-            array.append([f.text.strip().replace("\xa0\n\t", "") for f in link.find_all("td")])
-            print(counter)
-        else:
-            pass
-    print(array)
-    for i in range(len(array)):
-        f.writerow([array[i]])
-    df = pd.DataFrame()
-    df["Data"] = array
-    return df
-# df = OutputHTMLFileSummary2(url1,"tr",output_file6)
-# df = df.drop([0])
-# df = df.replace({'[':''}, regex=True)
-# print(df)
-# for i in range(len(df)):
-#     df.iloc[i] = df.iloc[i].replace("[","")
-#     df.iloc[i] = df.iloc[i].replace("]","")
-#     print(df.iloc[i])
+        # list.append([f.text.strip().replace("\xa0\n\t", "") for f in link.find_all("td")])
+        lists.append([f.text.strip().replace("\xa0\n\t", "") for f in link.find_all("td")])
+    for i in range(len(lists)):
+        print(i)
+        if i/2 != 0 and i>1:
+            lists[i][1] = lists[i][1].strip('"')
+            df.loc[i] = lists[i]
+            df.to_csv(output_file6)
+    print(df)
 
-
-# df.count
-# df.to_csv("output.csv",index = False)
-with open("output.csv","r", encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile)
-    new_list = []
-    with open("output2.csv", "a", encoding="utf-8") as csvfile2:
-        f = csv.writer(csvfile2)
-        new_list =[]
-        new_list2 =[]
-
-        for row in reader:
-            new_row = row[0]
-            new_row = row[0].replace("[","")
-            new_row = new_row.replace("]","")
-            new_row = new_row.replace('"',"")
-            new_list = new_row.split(",")
-            print(new_row)
-            for i in range(len(new_list)):
-                new_list[i] = new_list[i].replace("'",'')
-                new_list[i] = new_list[i].replace('\\','')
-            print(len(new_list))
-
-print("done")
-
-# with open("output2.csv","r", encoding="utf-8") as csvfile3:
-#     reader = csv.reader(csvfile3)
-#     for row in reader:
-#         print(row)
+OutputCardList(url1,"tr",output_file6)
